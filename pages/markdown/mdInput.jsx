093263058256b59ab8fae913be8754b697507299
kishore007k <kishorekumarstar07@gmail.com>
2021-09-02 18:32:35 +0530

    removed error from the create markdown

--- pages/markdown/mdInput.jsx
+++ pages/markdown/mdInput.jsx
@@ -17,6 +17,8 @@ const MdInput = () => {
 
 	const [editMode, setEditMode] = useState(true);
 
+	const token = window.localStorage.getItem("token");
+
 	const config = {
 		headers: {
 			Authorization: `Bearer ${token}`,
